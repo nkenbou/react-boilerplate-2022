73a4866595ec75022dadb24a1a1a0f8b91ac5c73
Kenichi Sone <k-sone@crestec.co.jp>
2022-09-01 08:32:55 +0900

    jest mock settings

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/fileMock.ts",
+    "\\.(sass|scss|css)$": "identity-obj-proxy",
+  },
 };
