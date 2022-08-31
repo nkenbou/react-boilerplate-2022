835a39d32de779825d86b3ce081e8a4e616f28c1
Kenichi Sone <k-sone@crestec.co.jp>
2022-08-31 18:06:47 +0900

    add webpack chunk's settings

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,31 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  entry: {
+    main: [path.join(__dirname, "src", "index.tsx")],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    chunkFilename: "[name].[contenthash].chunk.js",
+  },
+  optimization: {
+    chunkIds: "natural",
+    splitChunks: {
+      chunks: "initial",
+      filename: "[id].[contenthash].chunk.js",
+      cacheGroups: {
+        vendors: {
+          enforce: true,
+        },
+      },
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`,
+    },
+  },
   module: {
     rules: [
       {
@@ -21,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HTMLPlugin({
+    new HtmlWebpackPlugin({
       template: path.join(__dirname, "src/index.html"),
     }),
   ],
