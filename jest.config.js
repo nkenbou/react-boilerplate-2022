8b96992640d7141ad281241eaea3a17af4b758fe
Kenichi Sone <k-sone@crestec.co.jp>
2022-09-01 17:12:18 +0900

    add @storybook/addon-storyshots

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/src/__mocks__/fileMock.ts",
     "\\.(sass|scss|css)$": "identity-obj-proxy",
+    "\\.mdx?$": "<rootDir>/src/__mocks__/mdxMock.ts",
   },
 };
